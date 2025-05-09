d0235c8dce01fc06ee947bde503cacd8206cd986
pookee777 <71640721+pookee777@users.noreply.github.com>
2025-05-09 17:48:42 +0530

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,11 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
-db = SQLAlchemy(model_class=Base)
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
