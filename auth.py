db6263c2e84deee828b7688698d17137d2815798
pookee777 <71640721+pookee777@users.noreply.github.com>
2025-05-09 18:19:31 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from extensions import db
 from models import User, UserRole
